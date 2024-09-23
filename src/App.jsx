0586b3280c0786ee4ce176f599c825ecba69ac1c
mibrahimy <ebraheem@outlook.com>
2024-09-23 21:23:41 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/design/Hero";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        {/* <Benefits /> */}
+        {/* <Collaboration /> */}
+        {/* <Services /> */}
+        {/* <Pricing />
+        <Roadmap />
+        <Footer /> */}
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
